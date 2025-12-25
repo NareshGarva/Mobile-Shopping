bc4e0fab9a2c873fed0bbb08c469a34011a18ef8
Naresh Garva <garvanaresh1977@gmail.com>
2025-12-25 16:09:38 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,26 +1,14 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'mysql',
-      dialectOptions: {
-        ssl: { require: true },
-      },
-      logging: false,
-    })
-  : new Sequelize(
-      process.env.DATABASE,
-      process.env.DATABASE_USER,
-      process.env.DATABASE_PASSWORD,
-      {
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        dialect: 'mysql',
-        dialectOptions: {
-          ssl: { require: true },
-        },
-        logging: false,
-      }
-    );
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true, // use system CA
+    },
+  },
+  logging: false,
+});
 
 module.exports = sequelize;
