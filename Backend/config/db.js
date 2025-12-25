5fd3d615fe686c0d3370a48119f26929a408bfde
Naresh Garva <garvanaresh1977@gmail.com>
2025-12-25 16:38:07 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,11 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize({
   dialect: "mysql",
+  host: process.env.TIDB_HOST,
+  port: 4000,
+  username: process.env.TIDB_USER,
+  password: process.env.TIDB_PASSWORD,
+  database: process.env.TIDB_DATABASE,
   dialectOptions: {
     ssl: {
-      require: true,
-      rejectUnauthorized: true, //
+      minVersion: "TLSv1.2",
     },
   },
   logging: false,
